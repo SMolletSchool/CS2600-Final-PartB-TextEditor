a2110c17a309055000bde64252c25f80eee0df9d
SMolletSchool <shmollet@cpp.edu>
2021-11-29 23:08:21 -0800

    step 14

--- src/kilo.c
+++ src/kilo.c
@@ -26,8 +26,8 @@ int main() {
     rawMode();
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-        if (iscntrl(c)) printf("%d\n", c);
-        else printf("%d ('%c')\n", c, c);
+        if (iscntrl(c)) printf("%d\r\n", c);
+        else printf("%d ('%c')\r\n", c, c);
     }
     return 0;
 }
