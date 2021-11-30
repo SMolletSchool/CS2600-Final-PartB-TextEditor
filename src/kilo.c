a6b110a3ff0f06fd922edc2e553c59afe0b0d52c
SMolletSchool <shmollet@cpp.edu>
2021-11-29 23:28:19 -0800

    step 18

--- src/kilo.c
+++ src/kilo.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -12,11 +13,11 @@ void iskill(const char *s) {
 }
 
 void unrawMode() {
-    tcsetattr(STDIN_FILENO,TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO,TCSAFLUSH, &orig_termios) == -1) iskill("tcsetattr");
 }
 
 void rawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) iskill("tcgetattr");
     atexit(unrawMode);
 
     struct termios raw = orig_termios;
@@ -27,7 +28,7 @@ void rawMode() {
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) iskill("tcsetattr");
     
 }
 
@@ -35,7 +36,7 @@ int main() {
     rawMode();
     while (1) {
         char c = '\0';
-        read(STDIN_FILENO, &c, 1);
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) iskill("read");
         if (iscntrl(c)) printf("%d\r\n", c);
         else printf("%d ('%c')\r\n", c, c);
         if (c == 'q') break;
