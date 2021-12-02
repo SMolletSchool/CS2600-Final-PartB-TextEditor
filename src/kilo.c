982be6adcb36329902c406ccb780ecc919951c46
SMolletSchool <shmollet@cpp.edu>
2021-12-01 20:25:01 -0800

    step 27

--- src/kilo.c
+++ src/kilo.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 #include <termios.h>
 
@@ -16,8 +17,6 @@ struct editorConf {
 };
 
 struct editorConf E;
-
-
 
 //terminal
 void iskill(const char *s) {
@@ -53,6 +52,17 @@ char editorKey() {
     char c;
     while ((nread = read(STDIN_FILENO, &c, 1)) != 1) if (nread == -1 && errno != EAGAIN) iskill("read");
     return c;
+}
+
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+
+    if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) return -1;
+    else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
 }
 
 void editorKeyProcess() {
