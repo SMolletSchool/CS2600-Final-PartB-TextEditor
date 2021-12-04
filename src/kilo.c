0a585be33b9bfd9cd1eab6a96da55d8b12b207cc
SMolletSchool <shmollet@cpp.edu>
2021-12-04 00:36:21 -0800

    step fallout

--- src/kilo.c
+++ src/kilo.c
@@ -289,12 +289,15 @@ void editorRefreshScreen() {
 //input
 
 void editorMoveCursor(int key) {
+    erow *row = (E.cy >= E.numrows) ? NULL: &E.row[E.cy];
+
     switch (key) {
         case ARROW_LEFT:
             if (E.cx != 0)
             E.cx--;
             break;
         case ARROW_RIGHT:
+            if (row && E.cx < row ->size)
             E.cx++;
             break;
         case ARROW_UP:
