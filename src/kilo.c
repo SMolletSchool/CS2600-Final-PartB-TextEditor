ac9c297a9b56815c065f849a8cfc23f0aec7695b
SMolletSchool <shmollet@cpp.edu>
2021-12-03 23:33:47 -0800

    step 58

--- src/kilo.c
+++ src/kilo.c
@@ -156,18 +156,28 @@ int getWindowSize(int *rows, int *cols) {
 
 //file io
 
-void editorOpen() {
-    char *line = "Hello World.";
-    ssize_t linelen = 13;
+void editorOpen(char *filename) {
+    FILE *fp = fopen(filename, "r");
+    if (!fp) iskill("fopen");
 
-    E.row.size = linelen;
-    E.row.chars = malloc(linelen + 1);
-    memcpy(E.row.chars, line, linelen);
-    E.row.chars[linelen] = '\0';
-    E.numrows = 1;
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    linelen = getline(&line, &linecap, fp);
+    if (linelen != -1) {
+        while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+            linelen--;
+        E.row.size = linelen;
+        E.row.chars = malloc(linelen + 1);
+        memcpy(E.row.chars, line, linelen);
+        E.row.chars[linelen] = '\0';
+        E.numrows = 1;
+    }
+    free(line);
+    fclose(fp);
 }
 
-//buffer append
+//append buffer
 
 struct abuf {
     char *b;
@@ -307,10 +317,11 @@ void initEditor() {
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) iskill("getWindowSize");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     enableRawMode();
     initEditor();
-    editorOpen();
+    if (argc >= 2)
+    editorOpen(argv[1]);
 
     while (1) {
         editorScreenRef();
