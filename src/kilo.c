731dee369271531cab66bdd6586910e9757e8f51
SMolletSchool <shmollet@cpp.edu>
2021-12-03 21:35:18 -0800

    lex luthor stole 40 cakes. that's as many as 4 tens. and that's terrible.

--- src/kilo.c
+++ src/kilo.c
@@ -131,6 +131,7 @@ void editorRows(struct abuf *ab) {
     for (y = 0; y < E.screenrows; y++) {
         abAppend(ab, "`", 1);
 
+        abAppend(ab, "\x1b[K", 3);
         if (y < E.screenrows - 1) {
             abAppend(ab, "\r\n", 2);
         }
@@ -141,7 +142,6 @@ void editorScreenRef() {
     struct abuf ab = ABUF_INIT;
 
     abAppend(&ab, "\x1b[?25l", 6);
-    abAppend(&ab, "\x1b[2]", 4);
     abAppend(&ab, "\x1b[H", 3);
 
     editorRows(&ab);
