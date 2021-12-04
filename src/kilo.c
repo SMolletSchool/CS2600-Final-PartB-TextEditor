024da40e9738f8c1499e9787acb5064e19b0b5f0
SMolletSchool <shmollet@cpp.edu>
2021-12-03 23:19:34 -0800

    step 56

--- src/kilo.c
+++ src/kilo.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <termios.h>
 
@@ -153,6 +154,19 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
+//file io
+
+void editorOpen() {
+    char *line = "Hello World.";
+    ssize_t linelen = 13;
+
+    E.row.size = linelen;
+    E.row.chars = malloc(linelen + 1);
+    memcpy(E.row.chars, line, linelen);
+    E.row.chars[linelen] = '\0';
+    E.numrows = 1;
+}
+
 //buffer append
 
 struct abuf {
@@ -288,6 +302,8 @@ void initEditor() {
 int main() {
     enableRawMode();
     initEditor();
+    editorOpen();
+
     while (1) {
         editorScreenRef();
         editorProcessKeypress();
