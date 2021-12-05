46969b3434fbb7d9ede6060e694a8efedc60739a
SMolletSchool <shmollet@cpp.edu>
2021-12-04 16:18:35 -0800

    step 127

--- src/kilo.c
+++ src/kilo.c
@@ -69,6 +69,7 @@ struct editorConfig E;
 
 void editorSetStatusMessage(const char *fmt,...);
 void editorRefreshScreen();
+char *editorPrompt(char *prompt);
 
 //terminal
 void iskill(const char *s) {
@@ -358,7 +359,9 @@ void editorOpen(char *filename) {
 }
 
 void editorSave() {
-    if (E.filename == NULL) return;
+    if (E.filename == NULL) {
+        E.filename = editorPrompt("Save as: %s");
+    }
 
     int len;
     char *buf = editorRowsToString(&len);
